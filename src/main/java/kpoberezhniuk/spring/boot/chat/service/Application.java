86bf2150cf992c1f5d38f477c73be233ee72afa3
kpoberezhniuk <kpoberezhniuk@playtika.com>
2021-03-11 18:06:46 +0200

    my project

--- src/main/java/kpoberezhniuk/spring/boot/chat/service/Application.java
+++ src/main/java/kpoberezhniuk/spring/boot/chat/service/Application.java
@@ -1,25 +1,12 @@
 package kpoberezhniuk.spring.boot.chat.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    @Autowired
-    private PersonRepository personRepository;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        personRepository.deleteAll();
-        personRepository.save(new Person());
-        personRepository.save(new Person());
-        personRepository.save(new Person());
     }
 }
